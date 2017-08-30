ed78eaf9bb52f104e5712053514448a8cf30a85a
chat-wane <grumpy.chat.wane@gmail.com>
2017-08-30 16:50:28 +0200

    protocol

---
+++ src/main/java/descent/causality/protocols/RPSBasedCausalBroadcast.java
@@ -0,0 +1,8 @@
+package descent.causality.protocols;
+
+import descent.spray.Spray;
+import peersim.edsim.EDProtocol;
+
+public class RPSBasedCausalBroadcast extends Spray implements EDProtocol{
+
+}
