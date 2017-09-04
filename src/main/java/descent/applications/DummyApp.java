1474d2433f33232c17dfb56d50af0ba2e4e8832f
chat-wane <grumpy.chat.wane@gmail.com>
2017-09-04 17:10:22 +0200

    config file event based

--- src/main/java/descent/applications/DummyApp.java
+++ src/main/java/descent/applications/DummyApp.java
@@ -3,7 +3,9 @@ package descent.applications;
 /**
  * Dummy app doing nothing on message delivery.
  */
-public class DummyApp implements IApplication{
+public class DummyApp implements IApplication {
+
+	// (TODO) send empty messages at random date.
 
 	public void deliver(Object message) {
 		// nothing
